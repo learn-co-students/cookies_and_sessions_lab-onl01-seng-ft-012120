93631d682a33b2ada97f1ca7bbb324bd27378962
thedannysoto <87dsoto@gmail.com>
2020-04-05 19:05:35 -0500

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+require 'pry'
+class ProductsController < ApplicationController 
+helper_method :cart
+
+    def index 
+    end
+
+    def create
+        cart << params[:product]
+        redirect_to products_path
+    end
+end
